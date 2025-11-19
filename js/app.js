c522bf0baa83edded625b6b09334321a688bcb33
Jesus HM <jesus.h.mora0406@gmail.com>
2025-11-19 11:45:49 -0600

    feat: add basic addTask function

--- js/app.js
+++ js/app.js
@@ -11,3 +11,27 @@ const addBtn = document.getElementById('addBtn');
 const filterBtns = document.querySelectorAll('.filter-btn');
 const deleteBtns = document.querySelectorAll('.delete-btn');
 const clearBtn = document.getElementById('clearCompleted');
+
+function genereteId(){
+  return Date.now();
+};
+
+function addTask(){
+  const todoName = todoInput.value;
+
+  if (todoName.trim() === '') return;
+
+  if (typeof todoName !== 'string') return;
+
+  const todoExists = todos.some(t => t.name === todoName.trim());
+
+  if (todoExists) return;
+
+  const newTodo = { id: genereteId(), name: todoName.trim(), isCompleted: false };
+
+  todos.push(newTodo);
+
+  todoInput.value = '';
+};
+
+addBtn.addEventListener('click',addTask);
