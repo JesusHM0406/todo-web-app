004339f7e928f06637ca91b2439b20ca8113232e
Jesus HM <jesus.h.mora0406@gmail.com>
2025-11-23 18:54:29 -0600

    test: add full suite for deleteTodo function

--- tests/spec/todoTest.js
+++ tests/spec/todoTest.js
@@ -1,4 +1,4 @@
-import { createTask, loadTodos, todos } from "../../js/todo.js";
+import { createTask, deleteTodo, loadTodos, todos } from "../../js/todo.js";
 
 describe('createTask', ()=>{
   beforeEach(()=>{
@@ -30,5 +30,24 @@ describe('createTask', ()=>{
       createTask('todo name')
     }).toThrowError('There is already a task with that name');
     expect(todos.length).toEqual(2);
+  });
+});
+
+describe('deleteTodo', ()=>{
+  beforeEach(()=>{
+    spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify([
+      { id: '123', name: 'todo name', isCompleted: false },
+      { id: '321', name: 'name todo', isCompleted: false }
+    ]));
+    spyOn(localStorage, 'setItem');
+    loadTodos();
+  });
+
+  it('returns true if it deletes the task', ()=>{
+    expect(deleteTodo('123')).toEqual(true);
+  });
+
+  it("returns false if the id doesn't exists in the todo list", ()=>{
+    expect(deleteTodo('ok')).toEqual(false);
   });
 });
